a9ee3b91dda7153d6004ac384d10779c7b23ee2d
吳元皓 <hpware3@gmail.com>
2025-02-11 07:52:11 +0800

    Add packages & made some progress.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import { storeData, getData } from "@/components/data";
+
 
 export default function Index() {
+  const [host, setHost] = useState<string>("");
+  if ()
   return (
     <View style={s.mainview}>
       <Text>Hi</Text>
+              
+      <View style={s.inputBox}>
+          <TextInput style={s.label} placeholder="Host" placeholderTextColor="black" value={pwd} onChangeText={setpwd} secureTextEntry={true}></TextInput>
+        </View>
     </View>
    );
 }
@@ -12,6 +21,20 @@ const s = StyleSheet.create({
   mainview: {
     alignItems: "center",
     textAlign: "center",
+  },
+  inputBox: {
+    width: 274,
+    height: 40,
+    backgroundColor: '#fff',
+    marginVertical: 10,
+    borderRadius: 5,
+    padding: 10,
+    alignSelf:"center",
     
-  }
-})
+  },
+  label: {
+    fontSize: 13,
+    color: 'black',
+    fontFamily: 'Inter',
+  },
+})  
---
+++ components/api/checkStatus.ts
@@ -0,0 +1,3 @@
+export async function checkStatus(host: string) {
+    const req = await fetch(`${host}`)
+}
